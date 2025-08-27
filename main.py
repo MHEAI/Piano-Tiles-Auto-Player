d816db41723e432a8b9dc526f0520b551a22bd41
= <m.haadi.ehsan@gmail.com>
2025-08-27 16:55:11 +1000

    Added faster clicking using win32api

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pyautogui
 from time import sleep
-sleep(2)
+import win32api 
+import win32con
 
+sleep(2)
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 while True:
     y_pos = 411
     if pyautogui.pixelMatchesColor(601,y_pos,(0, 0, 0)):
-        pyautogui.click(601,y_pos)
+        click(601,y_pos)
     if pyautogui.pixelMatchesColor(684,y_pos,(0, 0, 0)):
-        pyautogui.click(684,y_pos)
+        click(684,y_pos)
     if pyautogui.pixelMatchesColor(775,y_pos,(0, 0, 0)):
-        pyautogui.click(775,y_pos)
+        click(775,y_pos)
     if pyautogui.pixelMatchesColor(859,y_pos,(0, 0, 0)):
-        pyautogui.click(859,y_pos)
+        click(859,y_pos)

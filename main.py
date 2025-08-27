42ff1ca338c4d96b73ae1f28b7c91eae011f821e
= <m.haadi.ehsan@gmail.com>
2025-08-27 19:05:14 +1000

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import pyautogui
 from time import sleep
 import win32api 
 import win32con
-
+import keyboard
 sleep(2)
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-while True:
+while not keyboard.is_pressed('q'):
     y_pos = 411
-    if pyautogui.pixelMatchesColor(601,y_pos,(0, 0, 0)):
-        click(601,y_pos)
-    if pyautogui.pixelMatchesColor(684,y_pos,(0, 0, 0)):
-        click(684,y_pos)
-    if pyautogui.pixelMatchesColor(775,y_pos,(0, 0, 0)):
-        click(775,y_pos)
-    if pyautogui.pixelMatchesColor(859,y_pos,(0, 0, 0)):
-        click(859,y_pos)
+    x_pos = [601,684,775,859]
+    for x in x_pos:
+        if pyautogui.pixelMatchesColor(x,y_pos,(0,0,0)):
+            click(x, y_pos)

f1962b0ffa9657f6c6a9d62de2863a820e6aaa3d
= <m.haadi.ehsan@gmail.com>
2025-08-27 20:56:03 +1000

    Improved fucntionality through refactorisation

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import pyautogui
 from time import sleep
 import win32api 
 import win32con
+import win32gui
 import keyboard
-sleep(2)
+sleep(5)
 def click(x, y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+def main():
+        while not keyboard.is_pressed('q'):
+            y_pos = 411
+            x_pos = [601,684,775,859]
+            for x in x_pos:
+                if pyautogui.pixelMatchesColor(x,y_pos,(0,0,0)):
+                    click(x, y_pos)
 
-while not keyboard.is_pressed('q'):
-    y_pos = 411
-    x_pos = [601,684,775,859]
-    for x in x_pos:
-        if pyautogui.pixelMatchesColor(x,y_pos,(0,0,0)):
-            click(x, y_pos)
